a3560ad09bad857d4a340598fa6161acfa531f2e
Leslie Cruz <lesliecruz2@my.unt.edu>
2024-06-24 22:07:48 -0500

    created methods in class where user can add and view books from the library

--- main.py
+++ main.py
@@ -1,34 +1,46 @@
 # Notes:
-# passing the copy not the actual thing
-# What's inside the function doesn't exist to outside the function
+# The library is not inheriting from books
+# This type of class is more of a composition class which a library has books
 
 class book:
-    def __init__(self, title, author, status):
-        self.title = title
-        self.author = author
-        self.status = status
+    def __init__(self, title=None, author=None, status=None):
+        self.title = input("Title: ")
+        self.author = input("Author:")
+        self.status = input("Status: ")
 
-    def book_status(self):
-        print("Title: ", self.title)
-        print("Author: ", self.author)
-        print("Status: ", self.status)
+    def __str__(self):
+        return f"Title: {self.title}, Author: {self.author}, Status: {self.status}"
 
-class library(book):
-    def __init__(self, title, author, status):
-        super().__init__(title,author, status)
+
+class library:
+    def __init__(self):
         self.library_books = []
 
+    def adding_book(self, book):
+        self.library_books.append(book)
+
+    def viewing_library(self):
+        if self.library_books:
+            print("Books in the library: ")
+            for book in self.library_books:
+                print(book, "\n")
 
 
 
 def main():
-    print("Hi, welcome to Cozy Reads")
-    print("Choose from the menu bellow what you would like to do.")
-    print("1: Add book to library")
-    print("2: View libray")
-    option = input("Enter your choice: ")
+    temp = library()
+    while True:
+        print("Hi, welcome to Cozy Reads")
+        print("Choose from the menu below:")
+        print("1: Add book to library")
+        print("2: View libray")
+        option = int(input("Enter your choice: "))
 
-
+        if option == 1:
+            book_added = book()
+            temp.adding_book(book_added)
+        elif option == 2:
+            temp.viewing_library()
 
 
 if __name__ == "__main__":
